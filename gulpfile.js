ce9fbdf0980c05d3d508c3434e32162cfa919867
Bret Newman <snowkeeper@snowpi.org>
2015-10-20 04:29:53 -0400

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,26 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var babel = require('babelify');
+var babel = require("gulp-babel");
+
 
 /**
  * Build Tasks
  */
 
+gulp.task("jsx", function () {
+  return gulp.src("src/console.js")
+    .pipe(babel())
+    .pipe(gulp.dest("lib"));
+});
+
 gulp.task('scripts', function(){
 	var b = browserify();
 	b.transform(babel); // use the reactify transform
-	b.add('lib/console.js');
+	b.add('src/console.js');
 	return b.bundle()
 				.pipe(source('console.js'))
 				.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', [ 'scripts'])
+gulp.task('default', [ 'jsx', 'scripts'])
